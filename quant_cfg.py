d5fa0031cefe638a73f297213ea54ea76ca3c48f
Lyon Lu <lyonlu13@gmail.com>
2025-05-19 18:11:12 +0000

    quantize 4 bit and PEFT

--- quant_cfg.py
+++ quant_cfg.py
@@ -29,12 +29,12 @@ def get_quant_config_slm(model):
     q8_config = BaseQuantizeConfig(nbits=8, group_size=64)  
     
     for i in range(n_layers):
-        quant_config[f'model.layers.{i}.self_attn.q_proj'] = q8_config
+        quant_config[f'model.layers.{i}.self_attn.q_proj'] = q4_config
         quant_config[f'model.layers.{i}.self_attn.k_proj'] = q4_config
-        quant_config[f'model.layers.{i}.self_attn.v_proj'] = q8_config
+        quant_config[f'model.layers.{i}.self_attn.v_proj'] = q4_config
         quant_config[f'model.layers.{i}.self_attn.o_proj'] = q4_config
         
-        quant_config[f'model.layers.{i}.mlp.gate_proj'] = q8_config
+        quant_config[f'model.layers.{i}.mlp.gate_proj'] = q4_config
         quant_config[f'model.layers.{i}.mlp.up_proj'] = q4_config
         quant_config[f'model.layers.{i}.mlp.down_proj'] = q4_config
         
